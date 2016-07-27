b86325143b1a50ce96989000e7f58bbb777d2459
Nexuist <nexuister@gmail.com>
2016-07-27 16:12:46 -0400

    Created uploader class

--- test.py
+++ test.py
@@ -1,1 +1,4 @@
 import requests
+
+upload = requests.get("http://pdf2doc.com")
+print(upload.status_code)
---
+++ uploader.py
@@ -0,0 +1,14 @@
+import requests
+
+class Uploader:
+
+	def __init__(self, file_path):
+
+	def upload():
+
+	def convert():
+
+	def status():
+
+	def download(file_path):
+		
