16ad9b0fdfe6b87b6e158241d095cdf25d847673
Nexuist <nexuister@gmail.com>
2016-07-27 17:38:08 -0400

    Working on file ID generation

---
+++ test.js
@@ -0,0 +1,16 @@
+var guid = (function() {
+	var counter = 0;
+
+	return function(prefix) {
+		var guid = new Date().getTime().toString(32), i;
+
+		for (i = 0; i < 5; i++) {
+			guid += Math.floor(Math.random() * 65535).toString(32);
+		}
+
+		return (prefix || 'o_') + guid + (counter++).toString(32);
+	};
+});
+
+console.log(new Date().getTime());
+console.log(new Date().getTime().toString(32));
--- test.py
+++ test.py
@@ -1,8 +1,33 @@
-import requests, math, random
+import requests, math, random, time, string
 
 # upload = requests.get("http://pdf2doc.com")
 # print(upload.status_code)
 
+
+def toBase32(x):
+	# Heavily modified version of https://www.quora.com/How-do-I-write-a-program-in-Python-that-can-convert-an-integer-from-one-base-to-another/answer/Nayan-Shah?srid=uVDVH
+	result = ""
+	while x > 0:
+		result = string.printable[x % 32] + result
+		x //= 32
+	return result
+
+
+
+def frm(x, b):
+    """
+    Converts given number x, from base 10 to base b
+    x -- the number in base 10
+    b -- base to convert
+    """
+    assert(x >= 0)
+    assert(1< b < 37)
+    r = ''
+    import string
+    while x > 0:
+        r = string.printable[x % b] + r
+        x //= b
+    return r
 def sid():
 	chars = "0123456789abcdefghiklmnopqrstuvwxyz"
 	result = ""
@@ -11,9 +36,9 @@ def sid():
 		result += chars[char:char + 1]
 	return result
 def fid():
-	pass
+	timestamp = int(time.time() * 1000) # Python equivalent of new Date().getTime()
+	return toBase32(timestamp)
 
-print(len(sid()))
-print(sid())
-print(sid())
-print(sid())
+print(fid())
+print(fid())
+print(fid())
