be456efc488278c40e184113031519212f64a868
Nexuist <nexuister@gmail.com>
2016-07-27 19:08:10 -0400

    Uploader now generates SID and FID on init

--- test.py
+++ test.py
@@ -1,29 +1,5 @@
-import requests, math, random, time, string
+from uploader import Uploader
 
-# upload = requests.get("http://pdf2doc.com")
-# print(upload.status_code)
-
-
-def toBase32(x):
-	# Heavily modified version of https://www.quora.com/How-do-I-write-a-program-in-Python-that-can-convert-an-integer-from-one-base-to-another/answer/Nayan-Shah?srid=uVDVH
-	result = ""
-	while x > 0:
-		result = string.printable[x % 32] + result
-		x //= 32
-	return result
-
-def sid():
-	chars = "0123456789abcdefghiklmnopqrstuvwxyz"
-	result = ""
-	for x in xrange(16):
-		char = int(math.floor(random.random() * len(chars)))
-		result += chars[char:char + 1]
-	return result
-
-def fid():
-	uid = toBase32(int(time.time() * 1000)) # Python equivalent of new Date().getTime().toString(32)
-	for x in xrange(5):
-		uid += toBase32(int(math.floor(random.random() * 65535)))
-	return "o_" + uid + toBase32(1)
-
-print(fid())
+a = Uploader("test")
+print(a.sid)
+print(a.fid)
--- uploader.py
+++ uploader.py
@@ -1,22 +1,42 @@
-import requests
+import requests, math, random, time, string
 
 class Uploader:
 
 	def __init__(self, file_path):
 		self.file_path = file_path
-
+		self.sid = self.__sid()
+		self.fid = self.__fid()
 
 	def upload():
+		pass
 
 	def convert():
+		pass
 
 	def status():
+		pass
 
-	def download(file_path):
+	def download(self, file_path):
+		pass
 
+	def __base32(self, x):
+		# Heavily modified version of https://www.quora.com/How-do-I-write-a-program-in-Python-that-can-convert-an-integer-from-one-base-to-another/answer/Nayan-Shah?srid=uVDVH
+		result = ""
+		while x > 0:
+			result = string.printable[x % 32] + result
+			x //= 32
+		return result
 
-	def __base32(x):
+	def __sid(self):
+		chars = "0123456789abcdefghiklmnopqrstuvwxyz"
+		result = ""
+		for x in xrange(16):
+			char = int(math.floor(random.random() * len(chars)))
+			result += chars[char:char + 1]
+		return result
 
-	def __sid():
-
-	def __fid():
+	def __fid(self):
+		uid = self.__base32(int(time.time() * 1000)) # Python equivalent of new Date().getTime().toString(32)
+		for x in xrange(5):
+			uid += self.__base32(int(math.floor(random.random() * 65535)))
+		return "o_" + uid + self.__base32(1)
