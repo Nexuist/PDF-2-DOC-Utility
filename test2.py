f45e5285c870ed5acb1796eb5a9664c790e5b69a
Nexuist <nexuister@gmail.com>
2016-08-01 19:36:00 -0400

    Cleanup no longer needed files

--- test2.py
+++
@@ -1,8 +0,0 @@
-from upload import Upload
-
-test = Upload("Test.pdf")
-test.sid = "lbu1bomd9og3epzf"
-test.fid = "o_1ap1na2u8v4u16mu5bose218gd1"
-test.convert_result = "Test.doc"
-print("Starting download")
-print(test.download("Test.doc"))
