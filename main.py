efc3136cf56ef9c0179be1c2ce24747797cc14f4
Nexuist <nexuister@gmail.com>
2016-08-01 19:35:30 -0400

    Recreated pygubu UI in pure Tkinter code

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ import pygubu
 
 def main():
 	root = Tk()
+	root.title("The pdf2doc Utility")
 	root.resizable(width = False, height = False)
-	
+	main = Frame(width = 500, height = 200)
+	main.grid()
+	micro_label = Label(main, text = "Uploading...")
+	micro_label.grid(row = 1, sticky = "w", padx = 25, pady = 10)
+	micro_bar = Progressbar(main, length = 400)
+	micro_bar.grid(row = 2, padx = 25)
+	macro_label = Label(main, text = "Testing...")
+	macro_label.grid(row = 3, sticky = "w", padx = 25, pady = 10)
+	macro_bar = Progressbar(main, length = 400)
+	macro_bar.grid(row = 4, padx = 25, ipady = 10)
 	print("Ready")
 	root.mainloop()
 
