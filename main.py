849c323bdbd597a2dd47d198d4b78a8b20cf1343
Nexuist <nexuister@gmail.com>
2016-08-01 18:02:52 -0400

    Continuing UI changes

--- main.py
+++ main.py
@@ -5,13 +5,7 @@ import pygubu
 def main():
 	root = Tk()
 	root.resizable(width = False, height = False)
-	root.geometry("500x200")
-	frame = Frame(root)
-	frame.pack(fill = BOTH, expand = YES)
-	label = Label(frame, text = "Downloading document...")
-	label.pack(side = TOP)
-	progress = Progressbar(frame, length = 100)
-	progress.pack(side = BOTTOM)
+	
 	print("Ready")
 	root.mainloop()
 
