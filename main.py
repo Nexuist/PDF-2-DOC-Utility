75b6d7cc916b8aa2411e1f6479aecbc7b0d1c39a
Nexuist <nexuister@gmail.com>
2016-08-05 18:33:43 -0400

    Moved initial UI config to main

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from upload import Upload
 from response import Response
 import sys, os
 
-VERBOSE = True
+global VERBOSE
+VERBOSE = False
 
 def debug(msg):
 	if not VERBOSE:
@@ -18,26 +19,42 @@ def debug(msg):
 	else:
 		print msg
 
-
-r = Response(None)
-debug(r)
-
 def main():
-	if len(sys.argv) != 2 or not sys.argv[1].lower().endswith(".pdf"):
+	if len(sys.argv) < 2 or not sys.argv[1].lower().endswith(".pdf"):
 		ui.info("Drag a PDF on top of the application to begin converting it.")
 	elif not os.path.isfile(sys.argv[1]):
 		ui.error("Incompatible File", "The file that was dragged onto the application cannot be converted. Make sure it is in PDF format.")
+	if len(sys.argv) >= 2 and sys.argv[2] == "-v":
+		global VERBOSE
+		VERBOSE = True
+		debug("Verbose mode activated")
 	file_name = sys.argv[1]
+	debug("Using file name: %s" % file_name)
 	upload = Upload(file_name)
+	ui.set_micro("Ensuring connectivity...", 0)
+	ui.set_macro("(1/3) Uploading PDF... ", 33)
 	if not upload.online():
 		ui.error("Network Failure", "Couldn't reach http://pdf2doc.com - Perhaps the website or your Internet is down.")
 	ui.set_micro("Website online. Beginning upload...", 0)
-	upload_size = 1
 	def progress(monitor):
 		text = "Uploading... %s / %s bytes" % (monitor.bytes_read, monitor.len)
 		percent = (monitor.bytes_read / monitor.len) * 100
 		ui.set_micro(text, percent)
 	response = upload.upload(progress)
-# ui = UI()
-# ui.root.after(500, main)
-# ui.render()
+	debug("UPLOAD:")
+	debug(response)
+	if response.error:
+		ui.error("Upload Failure", "Error: %s" % response.error)
+	ui.set_macro("(2/3) Converting...", 33)
+	ui.set_micro("Requesting conversion...", 0)
+	response = upload.convert()
+	debug("CONVERT:")
+	debug(response)
+	if response.error:
+		ui.error("Conversion Failure", "Error: %s" % response.error)
+
+
+
+ui = UI()
+ui.root.after(500, main)
+ui.render()
